120122b9c190355541248bcc61890199c723c990
Lucas M. Dutra <terremoth@users.noreply.github.com>
2020-10-26 09:44:33 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,42 +1,47 @@
 const intervalAudioAmp = setInterval(() => {
 
-function amplifyMedia(mediaElem, multiplier) {
-    var context = new (window.AudioContext || window.webkitAudioContext),
-    
-    result = {
-        context: context,
-        source: context.createMediaElementSource(mediaElem),
-        gain: context.createGain(),
-        media: mediaElem,
-        amplify: function(multiplier) { result.gain.gain.value = multiplier; },
-        getAmpLevel: function() { return result.gain.gain.value; }
-    };
-    
-    result.source.connect(result.gain);
-    result.gain.connect(context.destination);
-    result.amplify(multiplier);
-    
-    return result;
-}
-  const header = document.querySelector("._1QUKR");
-  if (header) {
-    clearInterval(intervalAudioAmp);
-    const button = document.createElement("button");
+    function amplifyMedia(mediaElem, multiplier) {
+        var context = new (window.AudioContext || window.webkitAudioContext),
 
-    function handleVolumeRateStatus (rate) {
-      const volumeRate = Number(rate);
-      if(volumeRate === 1) {
-        button.innerHTML = "2x";
-        button.classList.add("twoTimesButton");
-        button.classList.remove("oneTimeButton");
-        localStorage.setItem("volumeRate", "1");
-      } else if (volumeRate === 2) {
-        button.innerHTML = "1x";
-        button.classList.add("oneTimeButton");
-        button.classList.remove("twoTimesButton");
-        localStorage.setItem("volumeRate", "2");
-      }
-      return volumeRate;
+        result = {
+            context: context,
+            source: context.createMediaElementSource(mediaElem),
+            gain: context.createGain(),
+            media: mediaElem,
+            amplify: function(multiplier) { result.gain.gain.value = multiplier; },
+            getAmpLevel: function() { return result.gain.gain.value; }
+        };
+
+        result.source.connect(result.gain);
+        result.gain.connect(context.destination);
+        result.amplify(multiplier);
+
+        return result;
+    }
+    
+    const header = document.querySelector("._1QUKR");
+    
+    if (header) {
+        
+        clearInterval(intervalAudioAmp);
+        const button = document.createElement("button");
+
+        function handleVolumeRateStatus (rate) {
+            const volumeRate = Number(rate);
+            
+            if (volumeRate === 1) {
+                button.innerHTML = "2x";
+                button.classList.add("twoTimesButtonAmp");
+                button.classList.remove("oneTimeButtonAmp");
+                localStorage.setItem("volumeRate", "1");
+              } else if (volumeRate === 2) {
+                button.innerHTML = "1x";
+                button.classList.add("oneTimeButtonAmp");
+                button.classList.remove("twoTimesButtonAmp");
+                localStorage.setItem("volumeRate", "2");
+            }
+            
+            return volumeRate;
     }
 
     function handleAudioVolumeRate (rate = 1) {
